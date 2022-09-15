22f679cd258a31292127d68fd7a7de21d592d0bd
genouf <gabriel.enouf@yahoo.fr>
2022-09-15 10:56:01 +0200

    data_race handled

--- philo/sources/philo.c
+++ philo/sources/philo.c
@@ -6,7 +6,7 @@
 /*   By: genouf <genouf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 09:22:16 by genouf            #+#    #+#             */
-/*   Updated: 2022/09/15 09:09:02 by genouf           ###   ########.fr       */
+/*   Updated: 2022/09/15 10:49:58 by genouf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,22 @@ void	check_death(t_group_philo *philos, t_parsed *entry)
 	int	i;
 
 	i = 0;
+	ft_usleep(2);
 	while (i < entry->philo_num && entry->ph_finished == 0)
 	{
+		pthread_mutex_lock(&entry->mass_start);
 		pthread_mutex_lock(&philos->philo[i].m_eat);
 		if (get_time() - philos->philo[i].last_eat
 			>= philos->philo[i].entry->time_to_die && philos->philo[i].eat_count != 0)
 		{
 			philos->philo[i].alive = 0;
 			pthread_mutex_unlock(&philos->philo[i].m_eat);
+			pthread_mutex_unlock(&entry->mass_start);
 			entry->ph_finished = 1;
 			philo_print("died", &philos->philo[i]);
 			return ;
 		}
+		pthread_mutex_unlock(&entry->mass_start);
 		pthread_mutex_unlock(&philos->philo[i].m_eat);
 		i++;
 	}
